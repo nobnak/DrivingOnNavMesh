d6e5fd9b0a8dd6cc524d3d6e29f08f0fa49f1855
Nobuyuki Nakata <nobu.nakata@gmail.com>
2017-06-28 21:00:07 +0900

    動的ナビメッシュ生成機能

---
+++ DynamicNavMesh/NavMeshSourceTag.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+namespace DrivingOnNavMesh {
+        
+    public class NavMeshSourceTag : MonoBehaviour {
+        [SerializeField]
+        int area = 0;
+
+        MeshFilter mf;
+
+        static List<NavMeshSourceTag> tags = new List<NavMeshSourceTag>();
+
+        void OnEnable() {
+            mf = GetMeshFilter ();
+            tags.Add (this);
+        }
+        void OnDisable() {
+            tags.Remove (this);
+        }
+        IEnumerable<NavMeshBuildSource> GetSources() {
+            if (mf != null) {
+                var source = new NavMeshBuildSource ();
+                source.shape = NavMeshBuildSourceShape.Mesh;
+                source.area = area;
+                source.sourceObject = mf.sharedMesh;
+                source.transform = transform.localToWorldMatrix;
+                yield return source;
+            }
+        }
+
+        public static void Collect(ref List<NavMeshBuildSource> list) {
+            list.Clear ();
+            foreach (var tag in tags)
+                list.AddRange (tag.GetSources ());
+        }
+
+        bool TryGetMeshFilter(out MeshFilter mf) {
+            return (mf = GetMeshFilter()) != null;
+        }
+        MeshFilter GetMeshFilter() {
+            return GetComponent<MeshFilter> ();
+        }
+    }
+}
---
+++ DynamicNavMesh/NavMeshUpdator.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+namespace DrivingOnNavMesh {
+        
+    public class NavMeshUpdator : MonoBehaviour {
+        [SerializeField]
+        Vector3 size = new Vector3 (100f, 100f, 100f);
+
+        AsyncOperation updateProcess;
+        List<NavMeshBuildSource> sources;
+        NavMeshData navMesh;
+        NavMeshDataInstance navMeshInstance;
+
+        #region Unity
+        void OnEnable() {
+            sources = new List<NavMeshBuildSource> ();
+            navMesh = new NavMeshData ();
+            navMeshInstance = NavMesh.AddNavMeshData (navMesh);
+            UpdateNavMesh (false);
+        }
+        IEnumerator Start () {
+            while (true) {
+                updateProcess = UpdateNavMesh (true);
+                yield return updateProcess;
+            }
+    	}
+        void OnDisable() {
+            navMeshInstance.Remove ();
+        }
+        #endregion
+
+        AsyncOperation UpdateNavMesh(bool asyncOperation = false) {
+            NavMeshSourceTag.Collect (ref sources);
+            var setting = NavMesh.GetSettingsByID (0);
+            var bounds = new Bounds (Quantizer.Quantize (transform.position, size), size);
+
+            if (asyncOperation) {
+                return NavMeshBuilder.UpdateNavMeshDataAsync (navMesh, setting, sources, bounds);
+            } else {
+                NavMeshBuilder.UpdateNavMeshData (navMesh, setting, sources, bounds);
+                return null;
+            }
+        }
+    }
+}
