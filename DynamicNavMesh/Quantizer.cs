307065ebab0789521c37ed4d56c6d31a3cbd0640
Nobuyuki Nakata <nobu.nakata@gmail.com>
2017-07-04 18:04:04 +0900

    クオンタイズクラスを登録

---
+++ DynamicNavMesh/Quantizer.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace DrivingOnNavMesh {
+        
+    public static class Quantizer {
+        public static Vector3 Quantize(Vector3 v, Vector3 quant) {
+            return new Vector3 (
+                quant.x * Mathf.Floor (v.x / quant.x),
+                quant.y * Mathf.Floor (v.y / quant.y),
+                quant.z * Mathf.Floor (v.z / quant.z));
+        }
+    }
+}
