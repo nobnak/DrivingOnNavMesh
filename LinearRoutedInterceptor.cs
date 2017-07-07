88a097036cb66270b0101e9b5d0711c4f3509850
Naka Nobuyuki <nobu.nakata@gmail.com>
2017-07-07 15:10:30 +0900

    インターフェイスが違うだけの従来の Router も用意

---
+++ LinearRoutedInterceptor.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+namespace DrivingOnNavMesh {
+
+    public class LinearRoutedInterceptor : AbstractRoutetedInterceptor {
+        protected Vector3 destination;
+
+        public LinearRoutedInterceptor(Animator anim, Transform tr, DrivingSetting drivingSetting)
+            : base(anim, tr, drivingSetting, new LinearRouter()) {
+        }
+
+        #region implemented abstract members of AbstractRoutetedInterceptor
+        protected override bool TryToStartNavigationTo(Vector3 destination) {
+            var source = _tr.position;
+            this.destination = destination;
+            return router.TryToStartRoute (source, destination);
+        }
+        protected override void UpdateTarget (Vector3 pointFrom, float t) {
+            SetTarget (destination);
+        }
+        #endregion
+    }
+}
---
+++ LinearRouter.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+namespace DrivingOnNavMesh {
+
+    public class LinearRouter : AbstractRouter {
+        #region implemented abstract members of AbstractRouter
+        protected override bool TryToFindPath (Vector3 pointFrom, Vector3 pointTo, out Vector3[] path) {
+            path = new Vector3[] { pointFrom, pointTo };
+            return true;
+        }
+        #endregion
+    }
+}
